95a0a289cd5b44e00ac965afc7ab5bf8c13bad1a
Hassan ツ <130797396+hassan-aamer@users.noreply.github.com>
2023-08-21 01:18:59 +0300

    create home page

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+// Menu
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.menu');
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+    menu.classList.toggle('move');
+    bell.classList.remove('active');
+}
+
+//Notification
+let bell = document.querySelector('.notification');
+document.querySelector('#bell-icon').onclick = () => {
+    bell.classList.toggle('active');
+}
