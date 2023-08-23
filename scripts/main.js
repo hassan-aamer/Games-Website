63aebde56e405d518bb73830016bc1e9940f0eb2
Hassan ツ <130797396+hassan-aamer@users.noreply.github.com>
2023-08-23 15:16:50 +0300

    New Update Page Home & download

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,40 @@ let bell = document.querySelector('.notification');
 document.querySelector('#bell-icon').onclick = () => {
     bell.classList.toggle('active');
 }
+
+//Swiper
+var swiper = new Swiper(".trending-content", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay:{
+        delay:5000,
+        disableOnInteraction:false,
+    },
+    breakpoints: {
+            640: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+            768: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+            1068: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    },
+});
+
+// Custom Scroll Bar
+window.onscroll = function() {mufunction()};
+function mufunction(){
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById('scroll-bar').style.width = scrolled + '%';
+}
